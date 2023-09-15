d06da02428c2b3831ce91e8ef5a1a83760a72e8d
SeanQuinn <Seanquinn917@gmail.com>
2023-09-15 09:08:44 -0400

    commit tree 5c821febc92832588ef6e016ee4e681289776c50

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    # need a method that detects an anagram 
+    # use .chars for each
+    # def match([])
+    def match
+        (words).each do |word|
+        broken = word.chars
+         binding.pry
+         return broken
+        end
+    end
+end
